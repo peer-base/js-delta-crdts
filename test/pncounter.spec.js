6bef5025394ee5a42cfff838a48592cf0177d24f
Pedro Teixeira <i@pgte.me>
2018-05-10 15:13:10 +0100

    enabled pncounter sync tests

--- test/pncounter.spec.js
+++ test/pncounter.spec.js
@@ -42,7 +42,7 @@ describe('pncounter', () => {
     })
   })
 
-  describe.skip('together', () => {
+  describe('together', () => {
     let GCounter = CRDT('gcounter')
 
     let replica1, replica2
