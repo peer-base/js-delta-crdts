c8436fcec68f2bfae656d9278365f69d28a723e9
Pedro Teixeira <i@pgte.me>
2019-01-25 09:40:22 +0000

    chore: adds test for remove and insertAt RGA mutations

--- test/rga-permutations.spec.js
+++ test/rga-permutations.spec.js
@@ -27,7 +27,7 @@ describe('rga permutations', function () {
     const r2 = RGA('id2')
     for (let i = 0; i < OP_COUNT_PER_NODE; i++) {
       deltas.push(r1.push('1-' + i))
-      deltas.push(r1.push('2-' + i))
+      deltas.push(r2.push('2-' + i))
     }
     for (let delta of deltas) {
       r2.apply(delta)
@@ -39,6 +39,71 @@ describe('rga permutations', function () {
   })
 
   it('all combinations lead to the same result', () => {
+    let iterations = 0
+    for (let deltas of combinations) {
+      const r = RGA('id3')
+      for (let delta of deltas) {
+        r.apply(transmit(delta))
+      }
+
+      expect(r.value()).to.deep.equal(expectedResult)
+
+      if (++iterations === MAX_ANALYZED) {
+        break
+      }
+    }
+  })
+
+  it('makes random mutations', () => {
+    RGA = CRDT('rga')
+    const deltas = []
+    const r1 = RGA('id1')
+    const r2 = RGA('id2')
+    for (let i = 0; i < OP_COUNT_PER_NODE; i++) {
+      const d1 = r1.push('1-' + i)
+      deltas.push(d1)
+      r2.apply(d1)
+
+      const d2 = r2.push('2-' + i)
+      deltas.push(d2)
+      r1.apply(d2)
+    }
+    for (let i = 0; i < OP_COUNT_PER_NODE; i++) {
+      let len = r1.value().length
+      let index = Math.floor(Math.random() * len)
+      deltas.push(r1.removeAt(index))
+
+      len = r1.value().length
+      index = Math.floor(Math.random() * len)
+      deltas.push(r1.insertAt(index, '1x-' + index))
+
+      len = r2.value().length
+      index = Math.floor(Math.random() * len)
+      deltas.push(r2.removeAt(index))
+
+      len = r2.value().length
+      index = Math.floor(Math.random() * len)
+      deltas.push(r2.insertAt(index, '2x-' + index))
+    }
+
+    for (let delta of deltas) {
+      r1.apply(delta)
+      r2.apply(delta)
+    }
+
+    expectedResult = r2.value()
+    expect(r1.value()).to.deep.equal(expectedResult)
+
+    const r3 = RGA('id3')
+    for (let delta of deltas) {
+      r3.apply(transmit(delta))
+    }
+    expect(r3.value()).to.deep.equal(expectedResult)
+
+    combinations = Combinations(deltas)
+  })
+
+  it('all mutation combinations lead to the same result', () => {
     let iterations = 0
     for (let deltas of combinations) {
       const r = RGA('id3')
